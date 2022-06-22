6b918f977b4b98481318ca5cc3d052644d4cd3c9
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-06-22 18:21:12 +0300

    commit

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import { ReturnComponentType } from 'types/ReturnComponentType'
+import style from './Users.module.scss'
+import user from 'assets/images/user.png'
 
 type UsersPropsType = {
 
@@ -8,7 +10,83 @@ type UsersPropsType = {
 export const Users: FC<UsersPropsType> = ({ }): ReturnComponentType => {
 	return (
 		<div>
-			Users
+			<div className={style.content}>
+				<h1 className={style.developers}>Developers</h1>
+				<form className={style.form}>
+					<input type='text' name='term' />
+					<select name='friend' >
+						<option value='null'>All</option>
+						<option value='true'>Only followed</option>
+						<option value='false'>Only unfollowed</option>
+					</select>
+					<button className={style.btn} type='submit'>
+						Find
+					</button>
+				</form>
+				<div className={style.pagination}>
+					<button className={style.paginationBtn}>PREV</button>
+					<span>1, 2, 3, 4</span>
+					<button className={style.paginationBtn}>NEXT</button>
+				</div>
+			</div>
+			<div className={style.items}>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+				<div className={style.item}>
+					<div className={style.image}>
+						<img src={user} />
+						<div className={style.name}>Name</div>
+						<button>Follow</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
