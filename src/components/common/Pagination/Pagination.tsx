51a3fddd5d13c5fbfcb57f91114f0d6b140ab0aa
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-09-14 14:09:30 +0300

    commit

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,8 +1,8 @@
 import React, { FC, memo, useEffect, useState } from 'react'
 import { EMPTY_STRING } from 'constants/base'
 import { ReturnComponentType } from 'types/ReturnComponentType'
-import style from './Pagination.module.scss'
 import { PaginationPropsType } from './types'
+import style from './Pagination.module.scss'
 
 export const Pagination: FC<PaginationPropsType> =
 	memo(({ totalItemsCount, count, currentPage, handleSetCurrentPageClick, portionSize = 9 }): ReturnComponentType => {
@@ -10,14 +10,30 @@ export const Pagination: FC<PaginationPropsType> =
 		const [portionNumber, setPortionNumber] = useState(1)
 
 		const pagesCount = Math.ceil(totalItemsCount / count)
-		const pages = []
+		const pages: number[] = []
+
+		for (let i = 1; i <= pagesCount; i++) {
+			pages.push(i)
+		}
+
 		const portionCount = Math.ceil(pagesCount / portionSize)
 		const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
 		const rightPortionPageNumber = portionNumber * portionSize
+		const pagesFiltered = pages.filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+		const pagesRender = pagesFiltered.map(page => {
 
-		for (let i = 1; i < pagesCount; i++) {
-			pages.push(i)
-		}
+			const onSetCurrentPageClick = (): void => handleSetCurrentPageClick(page)
+
+			return (
+				<button
+					key={page}
+					className={currentPage === page ? style.active : EMPTY_STRING}
+					onClick={onSetCurrentPageClick}
+				>
+					{page}
+				</button>
+			)
+		})
 
 		useEffect(() => {
 			setPortionNumber(Math.ceil(currentPage / portionSize))
@@ -29,25 +45,8 @@ export const Pagination: FC<PaginationPropsType> =
 
 		return (
 			<div className={style.pagination}>
-
 				{portionNumber > 1 && <button onClick={onDecreasePortionNumberClick}>&laquo;</button>}
-
-				{pages
-					.filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
-					.map(page => {
-
-						const onSetCurrentPageClick = (): void => handleSetCurrentPageClick(page)
-
-						return (
-							<button
-								key={page}
-								className={currentPage === page ? style.active : EMPTY_STRING}
-								onClick={onSetCurrentPageClick}>
-								{page}
-							</button>
-						)
-					})}
-
+				{pagesRender}
 				{portionCount > portionNumber && <button onClick={onIncreasePortionNumberClick}>&raquo;</button>}
 			</div>
 		)
