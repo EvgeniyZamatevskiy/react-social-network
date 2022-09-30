91440e7bd41aa37b39e4b588446d0a644952fb70
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-09-30 17:30:58 +0300

    Added a component in case there are no users

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect } from 'react'
-import { Loader, User } from 'components'
+import { Loader, User, Filtration, UsersEmpty } from 'components'
 import { ReturnComponentType } from 'types'
 import { useSelector } from 'react-redux'
 import { useAppDispatch, useTheme } from 'hooks'
@@ -7,8 +7,6 @@ import { getUsers } from 'store/asyncActions'
 import { selectIsAuth, selectIsLoadingUsers, selectTerm, selectUsers } from 'store/selectors'
 import { Navigate } from 'react-router-dom'
 import { Path } from 'enums'
-import { setTerm } from 'store/slices/users'
-import { Filtration } from '../../components/filtration'
 import style from './Users.module.scss'
 
 export const Users: FC = (): ReturnComponentType => {
@@ -31,10 +29,6 @@ export const Users: FC = (): ReturnComponentType => {
     dispatch(getUsers({term}))
   }, [term])
 
-  const handleSetTermChange = (debouncedValue: string): void => {
-    dispatch(setTerm(debouncedValue))
-  }
-
   if (!isAuth) {
     return <Navigate to={Path.LOGIN}/>
   }
@@ -45,7 +39,9 @@ export const Users: FC = (): ReturnComponentType => {
         <h2 className={`${style.title} ${isDarkTheme && style.titleDark}`}>Users</h2>
         <Filtration/>
         <div className={style.userContainer}>
-          {isLoadingUsers ? <Loader/> : usersRender}
+          {isLoadingUsers
+            ? <Loader/>
+            : usersRender.length ? usersRender : <UsersEmpty/>}
         </div>
       </div>
     </div>
